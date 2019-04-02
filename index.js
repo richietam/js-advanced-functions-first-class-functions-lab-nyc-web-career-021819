32bde08e6cd9a8da4a3c4c189b06593dfb1b1ba0
richietam <richardtam.ny@gmail.com>
2019-04-02 09:51:36 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+  return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = function (drivers) {
+
+  return [drivers[drivers.length-2], drivers[drivers.length-1]]
+
+}
+
+selectingDrivers = [
+  returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value
+    };
+};
+
+const fareDoubler = function (createFareMultiplier) {
+    return createFareMultiplier * 2
+}
+
+
+const fareTripler = function (createFareMultiplier) {
+  return createFareMultiplier * 3
+}
+
+const fetchSpecifiedDrivers = function(arrayOfDrivers, thing) {
+  return thing(arrayOfDrivers)
+};
